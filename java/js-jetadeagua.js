b4d49b5fc81a27096034f722d220c1c1a2e3584b
jetadeagua <107442857+jetadeagua@users.noreply.github.com>
2022-06-13 18:34:13 -0600

    Add files via upload

---
+++ java/js-jetadeagua.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+} 
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', ()=>{
+  navbarLinks.classList.toggle('active')
+})
